9397333d573ee68f5bc40644df338c57dad2d81f
Mirosław Zalewski <mz@miroslaw-zalewski.eu>
2017-04-16 19:40:52 +0200

    Switch to HTTPS

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://miroslaw-zalewski.eu'
+SITEURL = 'https://miroslaw-zalewski.eu'
 RELATIVE_URLS = False
 
 FEED_DOMAIN = SITEURL
