bb5787f00718f209f84abd5df89258a710916292
Mirosław Zalewski <mz@miroslaw-zalewski.eu>
2019-02-12 02:07:25 +0100

    Improve bio and projects

--- content/pages/main.md
+++ content/pages/main.md
@@ -4,4 +4,8 @@ save_as: index.html
 template: main
 hidden: True
 
-I am Mirosław Zalewski, software tester living in Poznań, Poland.
+I am Mirosław Zalewski - software tester and open source enthusiast.
+
+My goal is to go beyond what's simple and obvious. I'm constantly looking for ways to deepen my knowledge and sharpen my skills. I got my master's degree in sociology. Currently I work at Red Hat, where I spread knowledge about skilled testing and challenge my own technical abilities.
+
+I live in Poznań, Poland.
--- metadata.py
+++ metadata.py
@@ -20,7 +20,7 @@ PROJECTS = [
         'tags': ['Python 3', 'Angular', 'TypeScript', 'SCSS'],
         'description': """
             Web application that helps you build your own Pokémon team in any core series game.
-            <a href="https://github.com/mirzal/create-pokemon-team">Github</a>.
+            <a href="https://github.com/mirzal/create-pokemon-team">GitHub</a>.
         """,
         'starred': True,
     },
@@ -41,6 +41,7 @@ PROJECTS = [
         'tags': ['R', 'ggplot2', 'Shiny (HTML, CSS)', 'data wrangling', 'visualisation'],
         'description': """
             R Shiny application to explore changes in civic engagement index in various European countries over time.
+            <a href="https://github.com/mirzal/ESS-civic-engagement">GitHub</a>.
         """,
         'starred': False,
     },
@@ -50,7 +51,7 @@ PROJECTS = [
         'imagetitle': 'przepis-na-lo',
         'tags': ['Wordpress', 'PHP', 'HTML', 'CSS', 'writing'],
         'description': """
-            My discontinued blog focused on <a href="https://libreoffice.org/">LibreOffice</a> tips and tricks. It was relatively popular in Polish open source and academic communities, attracting about 20 000 unique visitors in peak months.
+            My discontinued blog focused on <a href="https://libreoffice.org/">LibreOffice</a> tips and tricks. It was relatively popular in Polish open source and academic communities, attracting about 20&nbsp;000 unique visitors in peak months.
         """,
         'starred': False,
     },
@@ -60,7 +61,8 @@ PROJECTS = [
         'imagetitle': 'blog',
         'tags': ['Pelican', 'Jinja2 (HTML, SCSS)', 'writing'],
         'description': """
-            My current blog, focused mainly on tech and testing.
+            My current blog, focused mainly on testing and tech.
+            <a href="https://github.com/mirzal/website">GitHub</a>.
         """,
         'starred': False,
     },
