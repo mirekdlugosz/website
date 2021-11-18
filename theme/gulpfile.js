8478a080366d576217333420ab3f2f9834794e65
Mirek Długosz <mirek@mirekdlugosz.com>
2021-11-18 10:00:40 +0100

    Update theme dependencies

--- theme/gulpfile.js
+++ theme/gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require("gulp");
-var sass = require("gulp-sass");
+var sass = require("gulp-sass")(require("node-sass"));
 var autoprefixer = require("gulp-autoprefixer")
 var cleanCSS = require("gulp-clean-css");
 var concat = require("gulp-concat");
